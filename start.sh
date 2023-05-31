27829df5e53b6a678917410b98f242c7099021a7
Zerbaib <hugo.hennetin@proton.me>
2023-05-31 11:55:15 +0200

    step 1 need to dev all the reste

---
+++ start.sh
@@ -0,0 +1,25 @@
+# Sudo mode
+sudo su
+
+# Get the lasted version
+apt update && apt upgrade -y
+
+# Install base
+apt install git -y
+apt install curl -y
+apt install wget -y
+apt install htop -y
+apt install nano -y
+apt install vim -y
+
+# Install prog for host your code
+apt install python3.11 -y
+apt install Node.js -y
+apt install nginx -y
+apt install openjdk-7-jdk
+apt install openjdk-11-jdk
+apt install openjdk-16-jdk
+apt install openjdk-19-jdk
+
+# Finish step 1
+clear
