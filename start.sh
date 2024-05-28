341017a029f4b7dd745caa0b3a65bb8159064292
Hugo Hennetin <hugo.hennetin@proton.me>
2024-05-28 22:21:31 +0200

    init server

--- start.sh
+++ start.sh
@@ -1,36 +1,29 @@
-# Sudo mode
-sudo su
+#!/bin/bash
 
-# Get the lasted version
-apt update && apt upgrade -y
+apt update && apt upgrade -y && apt autoremove -y
 
-# Install base
-apt install git -y
-apt install curl -y
-apt install wget -y
-apt install htop -y
-apt install nano -y
-apt install vim -y
-apt install screen -y
+if [ -c "git --version" ]; then
+    echo "git is already installed"
+else
+    apt install git -y
+fi
 
-# Install prog for host your code
-apt install python3.11 -y
-apt install python3-pip -y
-apt install Node.js -y
-apt install npm -y
-apt install nginx -y
-apt install openjdk-7-jdk -y
-apt install openjdk-11-jdk -y
-apt install openjdk-16-jdk -y
-apt install openjdk-19-jdk -y
+if [ -c "htop --v" ]; then
+    echo "htop is already installed"
+else
+    apt install htop -y
+fi
 
-# Finish step 1
-clear
+if [ -c "curl --version" ]; then
+    echo "curl is already installed"
+else
+    apt install curl -y
+fi
 
-# Get all file of this prog
-wget https://github.com/Zerbaib/DebianServerInstall/blob/main/other/repos.sh
-wget https://github.com/Zerbaib/DebianServerInstall/blob/main/other/minecraft.sh
+if [ -c "neofetch --version" ]; then
+    echo "neofetch is already installed"
+else
+    apt install neofetch -y
+fi
 
-# Start module
-./other/repos.sh
-./other/minecraft.sh
+do-release-upgrade
