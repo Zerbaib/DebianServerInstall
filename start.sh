aed3f1912b6bdc4b3b1539a07c0dacb19ec1354a
Hugo Hennetin <hugo.hennetin@proton.me>
2024-05-28 22:35:29 +0200

    patch

--- start.sh
+++ start.sh
@@ -5,25 +5,25 @@ apt-get update -qq && apt-get upgrade -qq -y && apt-get autoremove -qq -y
 if [ -c "git --version" ]; then
     echo "git is already installed"
 else
-    apt-get install git -qq -y
+    apt-get install git -y
 fi
 
 if [ -c "htop --v" ]; then
     echo "htop is already installed"
 else
-    apt-get install htop -qq -y
+    apt-get install htop -y
 fi
 
 if [ -c "curl --version" ]; then
     echo "curl is already installed"
 else
-    apt-get install curl -qq -y
+    apt-get install curl -y
 fi
 
 if [ -c "neofetch --version" ]; then
     echo "neofetch is already installed"
 else
-    apt-get install neofetch -qq -y
+    apt-get install neofetch -y
 fi
 
 neofetch
